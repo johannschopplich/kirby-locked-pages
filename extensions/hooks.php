249d44e8b64acda0230e974002ee3fb78081d2b1
Johann Schopplich <mail@johannschopplich.com>
2023-08-09 07:36:42 +0200

    refactor: replace `kirby()` global fn

--- extensions/hooks.php
+++ extensions/hooks.php
@@ -27,7 +27,8 @@ return [
     },
 
     'locked-pages.logout' => function () {
+        $kirby = App::instance();
         $key = LockedPages::SESSION_KEY;
-        kirby()->session()->data()->remove($key);
+        $kirby->session()->data()->remove($key);
     }
 ];
